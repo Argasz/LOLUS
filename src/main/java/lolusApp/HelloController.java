727c2ce5c06fd4aad192cd3a1538d75cdb99be5a
argas <argasz@gmail.com>
2018-04-18 17:24:15 +0200

    HttpHeader allow-origin test

--- src/main/java/lolusApp/HelloController.java
+++ src/main/java/lolusApp/HelloController.java
@@ -1,13 +1,17 @@
 package lolusApp;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
-    String hello() {
+    @RequestMapping(value="/hello", method=RequestMethod.GET)
+    String hello(HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
         return  "Hello";
     }
 
