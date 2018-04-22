469cfe5fda264688f78becceb57327b48bcd9c6d
Jens Plate <jens.plate@apohem.se>
2018-04-22 11:47:27 +0200

    Testar /jens med JSON

--- src/main/java/lolusApp/HelloController.java
+++ src/main/java/lolusApp/HelloController.java
@@ -1,7 +1,12 @@
 package lolusApp;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.StringWriter;
 
 @RestController
 public class HelloController {
@@ -12,8 +17,18 @@ public class HelloController {
     }
 
     @RequestMapping("/jens")
-    String jens() {
-        return "Jens är bäst - ingen protest.";
+    String jens() throws IOException {
+        //return "Jens är bäst - ingen protest.";
+        JsonFactory jFactory = new JsonFactory();
+        StringWriter sw = new StringWriter();
+        JsonGenerator jgen = jFactory.createGenerator(sw);
+        jgen.writeStartObject();
+        jgen.writeStringField("name", "Jens");
+        jgen.writeNumberField("age", 13);
+        jgen.writeStringField("gender", "hen");
+        jgen.writeEndObject();
+        jgen.close();
+        return sw.toString();
     }
 
     @RequestMapping("/erik")
