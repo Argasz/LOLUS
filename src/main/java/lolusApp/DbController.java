4532a12f57f62785f5aa0bbe549479022007a1d9
Aron Gassilewski <argasz@gmail.com>
2018-04-26 22:08:01 +0200

    Fortsatt testning för databaskontakt: dbController.getAllEvents hämtar alla events i databasen.
    @Crossorigin tillagt till alla metoder

--- src/main/java/lolusApp/DbController.java
+++ src/main/java/lolusApp/DbController.java
@@ -2,10 +2,13 @@ package lolusApp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/*TODO: Datatyper, måste kunna söka efter intervall av tid/plats i databasen.
+ *TODO:      Metoder för detta.*/
 
 @Controller
 public class DbController {
@@ -13,16 +16,21 @@ public class DbController {
     @Autowired
     private EventRepository eventRepository;
 
+
+    @CrossOrigin
     @GetMapping("/addEvent")
     public @ResponseBody String addEvent(@RequestParam String time, @RequestParam String location){
-        //{year:"",month:"",day:"", hour:"", minute:""}
-        //{lat:xx.xx, long:yy.yy}
         Event e = new Event(location, time);
         eventRepository.save(e);
 
-        //JSONIFY
 
-        return "Event added!";
+        return "\nEvent added!";
+    }
+
+    @CrossOrigin
+    @GetMapping("/getAllEvents")
+    public @ResponseBody Iterable<Event> getAllEvents (){
+        return eventRepository.findAll();
     }
 
 }
