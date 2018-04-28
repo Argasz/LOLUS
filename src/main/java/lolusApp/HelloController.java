39252827f375b3fa83284efb22727a12408250b6
Jens Plate <jens.plate@apohem.se>
2018-04-28 11:53:50 +0200

    Adderad /sql

--- src/main/java/lolusApp/HelloController.java
+++ src/main/java/lolusApp/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.sql.*;
 
 @CrossOrigin
 @RestController
@@ -36,6 +37,55 @@ public class HelloController {
 
     @RequestMapping("/erik")
     String erik() {
+
         return "Erik är bättre";
+    }
+
+    @GetMapping("/sql")
+    String sql() throws IOException, SQLException {
+
+        String url = "jdbc:mysql://mysql.dsv.su.se:3306/jepl4052";
+        String username = "jepl4052";
+        String password = "eaJohG3ezahm";
+
+        JsonFactory jFactory = new JsonFactory();
+        StringWriter sw = new StringWriter();
+        JsonGenerator jgen = jFactory.createGenerator(sw);
+
+        Connection conn = DriverManager.getConnection(url, username, password);
+        Statement stmt = conn.createStatement();
+
+        stmt.execute("INSERT INTO jepl4052.test (name) VALUES ('sql-programmmet')");
+
+        ResultSet s = stmt.executeQuery("SELECT * FROM jepl4052.test");
+
+        StringBuilder names = new StringBuilder();
+
+        int count = 0;
+
+        jgen.useDefaultPrettyPrinter();
+
+        jgen.writeStartObject();
+        jgen.writeArrayFieldStart("users");
+
+        while(s.next()) {
+
+            jgen.writeStartObject();
+            jgen.writeNumberField("id", s.getInt("id"));
+            jgen.writeStringField("name", s.getString("name"));
+            jgen.writeEndObject();
+
+            count++;
+        }
+
+        jgen.writeEndArray();
+
+        jgen.writeEndObject();
+        jgen.close();
+
+        stmt.close();
+
+        return "Done: " + count + " rows affected. JSON: <pre>" + sw.toString() + "</pre>";
+
     }
 }
