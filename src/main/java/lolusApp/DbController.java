91f4c57e51b4a0146ab0c5217efbc46446866ab9
Jens Plate <jens.plate@apohem.se>
2018-05-24 13:59:38 +0200

    Adding /getPolice method @param ?location=

--- src/main/java/lolusApp/DbController.java
+++ src/main/java/lolusApp/DbController.java
@@ -3,6 +3,11 @@ package lolusApp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -71,5 +76,23 @@ public class DbController {
             return rd;
         }
 
+    }
+
+    @CrossOrigin
+    @GetMapping("/getPolice")
+    public @ResponseBody String getEventsByTimeAndLoc (@RequestParam String location) throws IOException {
+
+        String urlToRead = "https://polisen.se/api/events?locationName=" + location;
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
     }
 }
